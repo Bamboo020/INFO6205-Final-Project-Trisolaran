fa09905b35d8edd8cc9cee200ef7d0c2e362a381
Bamboo020 <chenlimingqi@outlook.com>
2026-04-02 15:50:42 -0400

    Interface and Implementation

---
+++ src/Implementation/Node.java
@@ -0,0 +1,13 @@
+package Implementation;
+
+public class Node<T> {
+    public Node next;
+    public T value;
+    public Node(T value) {
+        this.value = value;
+    }
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
---
+++ src/Interface/StackInterface.java
@@ -0,0 +1,11 @@
+package Interface;
+
+public interface StackInterface<T> {
+    public boolean isEmpty();
+    public boolean isFull();
+    public void push(T o);
+    public T pop();
+    public T peek();
+    public int size();
+    public void clear();
+}
